f462d4e068c16ad0034b7328c68a6522cedcd76e
Daniel Einars <contact@dle.dev>
2020-01-15 07:14:12 +0100

    06-function-props completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header';
+import { Button } from './components/Button';
 
 
-
-function f() {
-
-}
-
-const User = ({email, name}: User) => {
-};
 const App: React.FC = () => {
+  
+  
   return (
     <>
       <Header
         title={'Hello'}
-        status={'loading'}
-        thing={12}
-        thing2={['item 1', 'item 2']}
-        thing3={{}}
-        thing4={{name: 'Daniel'}}
       />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Button
+        onClick={(text) => {
+          console.log(text);
+        }}
+      />
     </>
   );
 };
---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+type Props = {
+  // onClick: Function // function (lower-case "f") is not recognized. Topic of chapter 7.
+  // method returns a string
+  // onClick(): string // function which returns a string.
+  
+  // method which does not anything.
+  // onClick(value: string): void
+  
+  // most common way to type a function.
+  onClick: (text: string) => void;
+}
+
+
+export const Button: React.FC<Props> = ({onClick}) => {
+  return <button onClick={() => onClick("hi there")}>Click Me</button>;
+};
