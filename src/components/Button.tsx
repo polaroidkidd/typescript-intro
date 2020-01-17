685f02e8ab2c161abb266f23d577e9a6c7ce98be
Daniel Einars <contact@dle.dev>
2020-01-17 18:49:09 +0100

    moved Props declaration into arrow function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,10 @@ import React  from 'react';
 
 
 type Props = {
-  // React has its own set of Events, such as React.MouseEvent. This will accept events for all clicks (bad)
-  // onClick: (e: React.MouseEvent) => void;
-  
-  // Now it is specified to only accept events from a button click (good)
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
-  
-  // Basic input event for forms.
-  onChange?: (e: React.FormEvent<HTMLInputElement>) => void
 }
 
 
-export const Button: React.FC<Props> = ({onClick}) => {
-  return <button onClick={onClick}>Click Me</button>;
+export const Button = ({onClick, children}: Props) => {
+  return <button onClick={onClick}>{children}</button>;
 };
