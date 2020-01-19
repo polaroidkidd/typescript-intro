4304fd142551a11bbfa7a81e73f9ed0e98a25223
Daniel Einars <contact@dle.dev>
2020-01-19 18:51:28 +0100

    09-typing-useState minor clean up

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 export const Input = () => {
   // Using the union type brackets (<>) we can define a state to be either of type string OR null. This will still throw an error because "value" can not be null
-  const [name, setName] = useState<string | null>("");
+  const [name, setName] = useState("");
   
   return (
     <input value={name} onChange={e => setName(e.target.value)}/>
