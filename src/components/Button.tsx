b622e906fe9abff5928983f697eb7b9822a32bc0
Daniel Einars <contact@dle.dev>
2020-01-22 17:58:26 +0100

    14-generics completed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,8 @@ import React, { FC } from 'react';
 
 type Props = {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
-  // Because we are passing a string into the button component, this will work.
-  // However, down the line it will become difficult because you might want to pass a myriad of various
-  // components into other components.
-  // children: string;
-  
 }
 
-// Placing the Prop declaration into the arrow handles. Use this only when children are in the mix.
-// Otherwise keep on using the previous syntax.
 export const Button: React.FC<Props> = ({onClick, children}) => {
   return <button onClick={onClick}>{children}</button>;
 };
--- src/components/useClickOutside.tsx
+++ src/components/useClickOutside.tsx
@@ -2,37 +2,21 @@ import React, { useEffect } from 'react';
 
 
 const useClickOutside = (
-  ref: React.MutableRefObject<HTMLDivElement>,
+  ref: React.MutableRefObject<HTMLElement>,
   handler: (event: MouseEvent | TouchEvent) => void) => {
   useEffect(() => {
-    
     const listener = (event: MouseEvent | TouchEvent) => {
-      // If there is no ref, do nothing.
-      // If the ref contains the target (click on the ref), do nothing.
       if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
-      // Otherwise, fire the event (click outside the item)
-      // handler(event);
-      
-      
-      // Need to declare the event as a React.MouseEvent<HTMLElement> in the hook definition
       handler(event);
-      
-      
-      // This is a bug between JS and React. The only way to fix this is to
-      // declare the event.target coming in as Node
-      // if (!ref.current || ref.current.contains(event.target)) {
     };
-    
     document.addEventListener('mousedown', listener);
     document.addEventListener('touchstart', listener );
     return () => {
       document.removeEventListener('mousedown', listener);
       document.removeEventListener('touchstart', listener);
-      
     };
-    // only run if the handler or the ref has changed
   }, [handler, ref]);
 };
 
